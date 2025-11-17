36498697b0091790a587cb81404b878618f473dd
Elkin Darío González Martínez <elkindgonzalez@gmail.com>
2025-11-17 18:13:10 -0500

    Instalacion tema coreui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@coreui/coreui/dist/css/coreui.min.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+// Importación moderna de Bootstrap 5
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
