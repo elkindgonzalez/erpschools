480dae01f077eae27680bd92526bf9d0e267c5d3
Aldairbb <118485501+Aldairbb@users.noreply.github.com>
2025-11-17 21:13:36 -0500

    se crea definición de carpetas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import indexRoutes from "./presentation/routes/index.routes";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    /* origin: "https://erpschools.onrender.com", */
+    credentials: true
+}))
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend ERP Schools funcionando");
-});
+app.use(indexRoutes)
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
